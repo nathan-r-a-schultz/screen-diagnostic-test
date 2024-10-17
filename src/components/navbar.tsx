8873a5c5bc122c146381df7b5b060ea0939dbf45
Natratz <103611211+Natratz@users.noreply.github.com>
2024-10-16 22:19:33 -0400

    Added boilerplate page elements

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <p>This is the navbar!</p>
+  );
+};
+
+export default Navbar;

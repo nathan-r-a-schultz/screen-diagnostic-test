691f9fc4b56a40f78597356c4f044ff0f91b3162
Nathan R.A. Schultz <nathan.r.a.schultz@gmail.com>
2024-11-25 15:12:42 -0500

    Added menu before starting diagnostic test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,32 @@
 
 import React, { useState } from 'react';
 import TouchButton from "../components/touchbutton"
+import './globals.css';
 
 const Page: React.FC = () => {
   const [showTouchButton, setShowTouchButton] = useState(false);
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
 
   // Toggle the visibility of the TouchButton
   const handleToggleTouchButton = () => {
     setShowTouchButton(prev => !prev);
+    setIsButtonVisible(false);
   };
 
   return (
-    <div>
-      <button onClick={handleToggleTouchButton}>
-        {showTouchButton ? 'Hide TouchButton' : 'Show TouchButton'}
-      </button>
-
-      {showTouchButton && <TouchButton />}
+    <div className="center-container">
+      <div className="center-content">
+        <p className="center-text">This is a screen diagnostic test</p>
+        {isButtonVisible && (
+          <button className="menu-button" onClick={handleToggleTouchButton}>
+            {showTouchButton ? 'Hide TouchButton' : 'Start'}
+          </button>
+        )}
+        {showTouchButton && <TouchButton />}
+      </div>
     </div>
   );
+  
 };
 
 export default Page;
